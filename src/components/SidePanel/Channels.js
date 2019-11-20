1a7f2e22f17f70c7e468c471de194e261a4e4710
anuragmy <anuragmy2729@gmail.com>
2019-11-20 14:39:14 +0530

    add channel remaining

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,16 +1,58 @@
 import React, { Component } from "react";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
+import firebase from "../../firebase";
 
 class Channels extends Component {
   state = {
     channels: [],
+    user: this.props.currentUser,
     modal: false,
     channelName: "",
-    channelDetails: ""
+    channelDetails: "",
+    channelsRef: firebase.database().ref("channels")
   };
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addChannel = () => {
+    const { channelsRef, channelName, user } = this.state;
+    console.log("add channedl opened");
+    // const key = channelsRef.push().key;
+    // const newChannel = {
+    //   id: key,
+    //   name: channelName,
+    //   details: {
+    //     createdBy: {
+    //       name: user.displayName,
+    //       avatar: user.photoUrl
+    //     }
+    //   }
+    // };
+
+    // channelsRef
+    //   .child(key)
+    //   .update(newChannel)
+    //   .then(() => {
+    //     this.setState({ channelName: "", channelDetails: "" });
+    //     this.closeModal();
+    //     console.log("channel added");
+    //   })
+    //   .catch(err => console.log(err));
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      console.log("channel added");
+      this.setState({ [event.target.name]: event.target.value });
+      console.log(this.state);
+    }
+  };
+
+  isFormValid = ({ channelName, channelDetails }) => {
+    channelName && channelDetails;
   };
 
   closeModal = () => this.setState({ modal: false });
@@ -53,7 +95,7 @@ class Channels extends Component {
             </Form>
           </Modal.Content>
           <Modal.Actions>
-            <Button color="green" inverted>
+            <Button color="green" inverted onClick={this.handleSubmit}>
               <Icon name="checkmark" /> Add
             </Button>
             <Button color="red" inverted onClick={this.closeModal}>
