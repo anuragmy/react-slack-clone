819d2ddcec36705da4ff8c4de4c454231116ba08
Anurag <anuragmy2729@gmail.com>
2019-11-25 11:36:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,7 @@ import "firebase/database";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQsZ5PMVAlXaxL4q4aXg1rNgJbX_BXFmY",
-  authDomain: "react-clone-app.firebaseapp.com",
-  databaseURL: "https://react-clone-app.firebaseio.com",
-  projectId: "react-clone-app",
-  storageBucket: "react-clone-app.appspot.com",
-  messagingSenderId: "762078484922",
-  appId: "1:762078484922:web:a38d705de12c1ea3c8a583",
-  measurementId: "G-9WGKSK5381"
+  // your firebase configuration
 };
 
 firebase.initializeApp(firebaseConfig);
